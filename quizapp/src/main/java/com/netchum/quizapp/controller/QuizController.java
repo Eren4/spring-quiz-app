58e2c9c21a6c7d0ffb4a82456330bcaa86d972ea
Eren4 <erendenizkusu@gmail.com>
2023-09-03 09:05:07 +0300

    Created question html

--- quizapp/src/main/java/com/netchum/quizapp/controller/QuizController.java
+++ quizapp/src/main/java/com/netchum/quizapp/controller/QuizController.java
@@ -1,13 +1,48 @@
 package com.netchum.quizapp.controller;
 
+import com.netchum.quizapp.entity.Question;
+import com.netchum.quizapp.service.QuestionService;
+import com.netchum.quizapp.service.QuizTakerService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class QuizController {
 
+    @Autowired
+    private QuestionService questionService;
+
+    @Autowired
+    private QuizTakerService quizTakerService;
+
     @GetMapping("/")
     public String homePage() {
         return "home-page";
+    }
+
+    @PostMapping("/start")
+    public String startQuiz(@RequestParam("username") String username, HttpSession session) {
+        // Store the username and initialize progress (current question number) in the session or database
+        session.setAttribute("username", username);
+        session.setAttribute("currentQuestion", 1); // Start from the first question
+        return "redirect:/quiz/question"; // Redirect to the first question
+    }
+
+    @PostMapping("/question/{questionNumber}")
+    public String startQuiz(@PathVariable int questionNumber,
+                            @RequestParam("username") String username,
+                            Model model) {
+
+        List<Question> questions = questionService.getAllQuestions();
+
+
+        model.addAttribute("username", username);
+
+        return "question-page";
     }
 }
--- quizapp/src/main/java/com/netchum/quizapp/service/QuestionService.java
+++ quizapp/src/main/java/com/netchum/quizapp/service/QuestionService.java
@@ -1,8 +1,11 @@
 package com.netchum.quizapp.service;
 
+import com.netchum.quizapp.entity.Question;
 import com.netchum.quizapp.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class QuestionService {
@@ -12,5 +15,9 @@ public class QuestionService {
     @Autowired
     public QuestionService(QuestionRepository questionRepository) {
         this.questionRepository = questionRepository;
+    }
+
+    public List<Question> getAllQuestions() {
+        return questionRepository.findAll();
     }
 }
