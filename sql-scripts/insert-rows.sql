7f5abd79fb76c1c88ce7c1497b72ae0840e9ef16
Eren4 <erendenizkusu@gmail.com>
2023-09-03 08:21:48 +0300

    Inserted rows

---
+++ sql-scripts/insert-rows.sql
@@ -0,0 +1,17 @@
+insert into quiz_taker(username, score, date_registered)
+values('Joe', 50, '2022-05-14'),
+('Jack', 80, '2022-08-02'),
+('Eileen', 95, '2023-02-23');
+
+insert into question(question_text, option_0, option_1,
+option_2, option_3, correct_option_index)
+values('2 + 2 = ?', '0', '1', '2', '3', 2),
+('What is Java?', 'A car', 'A programming language',
+'A phone', 'A star', 1),
+('What is the star in our solar system called?',
+'Sirius', 'VV Cephei', 'Sun', 'Rigel', 2),
+('Who is the founder of The Republic of Türkiye?',
+'Mustafa Kemal Atatürk', 'Joe Biden', 'Enver Paşa',
+'Fevzi Çakmak', 0),
+('In which mythology does the underground god Erlik belong?',
+'Scandinavian', 'Greek', 'Aztec', 'Turkic', 3);
